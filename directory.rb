46fca189eace8afbb16df10a0103d042524e8ee0
Marek <MarekSchmidt@outlook.com>
2018-07-02 23:22:53 -0700

    Greatly changed directory.rb. Added functionality (formatting, control flow)  and added some challenges as new methods

--- directory.rb
+++ directory.rb
@@ -15,6 +15,8 @@ students = [
 ]
 =end
 
+#METHODS
+
 #Changed to get names from user instead
 def input_students
   puts "Please enter the names of the students"
@@ -35,21 +37,79 @@ def input_students
   students
 end
 
-#METHODS
 #Prints header that comes before student names
 def print_header
   puts "The students of Villains Academy"
   puts "-----------------"
 end
+
 #Prints all names from students array
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  if students.size <= 0
+    puts "Sorry, there are no students to display"
+  else
+    students.each_with_index do |student, index|
+      #Added formatting / justification to output
+      #Also added index number before each name
+      index_s = "#{index + 1}: "
+      name_s = "#{student[:name]}"
+      cohort_s = " #{student[:cohort]} cohort"
+      puts index_s.ljust(4) + name_s.ljust(32) + cohort_s.ljust(20)
+    end
   end
 end
+
+#CHALLENGES ADDED AS NEW METHODS
+
+#Asks user for a letter and returns all student info for student names beginning with that letter
+def print_names_starting_with(students)
+  puts "Which letter would you like to search for?"
+  letter = gets.chomp
+  match_array = []
+  students.each_with_index do |student, index|
+    #Checks input against capital and lowercase
+    if student[:name][0] == letter.upcase || student[:name][0] == letter.downcase
+      match_array.push(student)
+    end
+  end
+  print(match_array)
+  if match_array.length == 1
+    puts "We have found #{match_array.size} student whose name begins with #{letter.upcase}"
+  elsif match_array.length > 1
+    puts "We have found #{match_array.size} students whose names begin with #{letter.upcase}"
+  end
+end
+
+#Asks users for an integer x and prints all student info for students whose names are less than or equal to x characters long
+def print_names_less_than_x_characters(students)
+  puts "Please set the max character length of names to display"
+  number = gets.chomp
+  match_array = []
+  #Will not continue unless input is an integer, and avoided cases in which to_i for a string returns 0, AKA an integer
+  while number.to_i.to_s != number
+    puts "Please enter an integer"
+    number = gets.chomp
+  end
+  students.each_with_index do |student, index|
+    if student[:name].length <= number.to_i
+      match_array.push(student)
+    end
+  end
+  print(match_array)
+  if match_array.length == 1
+    puts "We have found #{match_array.size} student with a name of #{number} characters or less"
+  elsif match_array.length > 1
+    puts "We have found #{match_array.size} students with names of #{number} characters or less"
+  end
+end
+
 #Prints total number of students in directory
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  if students.count == 1
+    puts "Overall, we have #{students.count} great student"
+  elsif students.count > 1
+    puts "Overall, we have #{students.count} great students"
+  end
 end
 
 #CALLING EACH METHOD
@@ -57,3 +117,7 @@ students = input_students
 print_header
 print(students)
 print_footer(students)
+
+print_names_starting_with(students)
+
+print_names_less_than_x_characters(students)
